90e3186a2c5cc87fa3f64e10987b9dd75d8c49df
u15008739 <u5008739@tuks.co.za>
2018-04-07 16:07:26 +0200

    Neatend things up. Note TODO file. > Moved all the firbase script in html files to a seperate .js file. > Sign out now works. > All links within the website to completed pages now work. > Commented out broken links to not yet implemented features. > Removed all mention of username from sign in and register pages. > Added a confirm password field, with a check on the register page. > Deleted information.css stub.
    > Tomorrow, I will reformat all of the JavaScript and HTML to follow Google coding guidlines as specified in the doc, intercept me before I do that, if the guidlines will change.
    
    > This is now demoable, I believe.

---
+++ Source/Web/app/firebaseRelatedJavaScriptFunctions.js
@@ -0,0 +1,84 @@
+function initializeFirebase() {
+    var config = {
+        apiKey: "AIzaSyBTTgAtocwDfror-XZLi4R5rUEphTUK4PU",
+        authDomain: "harvest-ios-1522082524457.firebaseapp.com",
+        databaseURL: "https://harvest-ios-1522082524457.firebaseio.com",
+        projectId: "harvest-ios-1522082524457",
+        storageBucket: "harvest-ios-1522082524457.appspot.com",
+        messagingSenderId: "167365669216"
+    };
+    firebase.initializeApp(config);
+}
+
+initializeFirebase();// set up the database
+
+function refreshPage() {
+    location.reload();
+}
+
+//Old from testing, to be removed.
+/*function fbSignIn(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        alert("Sign In Fail");
+        // ...
+    });
+}*/
+
+// This is automatic whenever there's a change to a users authorization
+/*firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // user logged in
+        // window.location.href = "HomePage.html";
+    } else {
+        // user logged out
+        window.location.href = "Login.html";
+    }
+});*/
+
+function firebaseRegister() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    firebase.auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(function (user) { // user details correct
+            document.location.href = "HomePage.html";
+        })
+        .catch(function (error) { // some error occured
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+        });
+}
+
+function firebaseLogin() {
+    var email = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) { // user details correct
+        document.location.href = "HomePage.html";
+    }).catch(function (error) { // some error occured
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+    });
+}
+
+function signOut(){
+    firebase.auth().signOut().then(function () {
+        document.location.href="Login.html";
+    });
+}
