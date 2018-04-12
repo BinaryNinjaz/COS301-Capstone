5c957f1bd1c7588de6db741983356b3501c1d492
u15008739 <u5008739@tuks.co.za>
2018-04-12 17:38:15 +0200

    Tests for Register Done

--- Source/Web/app/register.js
+++ Source/Web/app/register.js
@@ -1,13 +1,20 @@
 function register() {
-  const pass = document.getElementById("password");
-  const pass2 = document.getElementById("passwordConf");
-  if (pass.value !== pass2.value) {
-    pass.value = "";
-    pass2.value = "";
-    document.getElementById("errorSpace").innerHTML = "<p class='errmsg'>Passwords do not match</p>";
-  }
-  else {
-    document.getElementById("errorSpace").innerHTML = "<br>";
-    firebaseRegister();
-  }
+    checkPass(document.getElementById("password").value, document.getElementById("passwordConf").value);
+}
+
+function checkPass(pass1, pass2) {
+    if (pass1 === "" || pass2 === "") {
+        document.getElementById("errorSpace").innerHTML = "<p class='errmsg'>Passwords cannot be empty</p>";
+        return false;
+    }
+    else if (pass1 !== pass2) {
+        document.getElementById("errorSpace").innerHTML = "<p class='errmsg'>Passwords do not match</p>";
+        document.getElementById("password").value = 0;
+        document.getElementById("passwordConf").value = 0;
+        return false;
+    }
+    if (pass1 === pass2) {
+        document.getElementById("errorSpace").innerHTML = "<br>";
+        return true;
+    }
 }
