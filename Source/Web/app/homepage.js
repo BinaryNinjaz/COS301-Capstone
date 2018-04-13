656ce4de959f7268b8997815fd23dbb6fbd95b79
Letanyan <letanyan.a@gmail.com>
2018-04-14 01:11:38 +0200

    display live location on home page

--- Source/Web/app/homepage.js
+++ Source/Web/app/homepage.js
@@ -1,30 +1,27 @@
-// A reference to the database location
-var yieldRef = firebase.database().ref('yields');
-// When a yield is added, changed or deleted
-yieldRef.on('value', function (snapshot) {
-  // snapshot is the part of the database that was changed
+const locationsRef = firebase.database().ref('/locations');
+var locations = [];
 
-  var yields = document.getElementById('yields');
-  yields.innerHTML = "";
-
-  // yields has multiple yield inputs
-  // so we iterate over each yield individually
-  snapshot.forEach(function (childSnapshot) {
-    // we use val() to get the actual data.
-    // the we can obj like it was just a normal object with fields
-    var obj = childSnapshot.val();
-
-    var date = obj.date;
-    var duration = obj.duration;
-    var email = obj.email;
-    var lat = obj.location.lat;
-    var lng = obj.location.lng;
-    var yieldAmount = obj.yield;
-
-    yields.innerHTML += "<b>Date: </b>" + date + "</br>";
-    yields.innerHTML += "<b>Duration: </b>" + duration + "</br>";
-    yields.innerHTML += "<b>Email: </b>" + email + "</br>";
-    yields.innerHTML += "<b>Location: </b>" + lat + "," + lng + "</br>";
-    yields.innerHTML += "<b>Yield: </b>" + yieldAmount + "</br></br>";
+var map;
+function initMap() {
+  navigator.geolocation.getCurrentPosition(function(loc) {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: loc.coords.latitude, lng: loc.coords.longitude },
+      zoom: 14
+    });
   });
-});
+}
+
+function displayForemanLocation() {
+  locationsRef.on('value', function(snapshot) {
+    locations = [];
+    snapshot.forEach(function (child) {
+      let loc = child.val();
+      
+      var marker = new google.maps.Marker({
+        position: loc.coord,
+        map: map
+      });
+    });
+  });
+}
+displayForemanLocation();
