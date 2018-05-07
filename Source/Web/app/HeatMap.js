353a23c76e8c2e6fb6f4f0a1e02daf34351ed86a
Letanyan Arumugam <letanyan.a@gmail.com>
2018-05-07 10:24:28 +0200

    heatmap works with new db layout (#128)
    * heatmap works with new db layout
    
    * draw map when user is known

--- Source/Web/app/HeatMap.js
+++ Source/Web/app/HeatMap.js
@@ -1,3 +1,21 @@
+const user = function() { return firebase.auth().currentUser };
+const userID = function() {
+  if (user() !== null) {
+    return user().uid 
+  } else {
+    return ""
+  }
+}
+function yieldsRef() {
+  return firebase.database().ref('/' + userID() + '/sessions');
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    initMap();
+  }
+});
+
 var map;
 function initMap() {
   navigator.geolocation.getCurrentPosition(function(loc) {
@@ -12,10 +30,11 @@ function initMap() {
   displayHeatMap();
 }
 
-const yieldsRef = firebase.database().ref('/yields');
+
+
 function displayHeatMap() {
-  yieldsRef.off();
-  yieldsRef.on('value', function(snapshot) {
+  yieldsRef().off();
+  yieldsRef().on('value', function(snapshot) {
     locations = [];
     snapshot.forEach(function (child) {
       let cols = child.val().collections;
