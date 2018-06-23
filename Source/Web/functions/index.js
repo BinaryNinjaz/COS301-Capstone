401ba44e2f0ab867c7d812e031356e66d41b4fee
Letanyan <letanyan.a@gmail.com>
2018-06-23 03:03:46 +0200

    get sessions with a certain date range

--- Source/Web/functions/index.js
+++ Source/Web/functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+//const cors = require('cors')({origin: true});
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.sessionsWithinDates = functions.https.onRequest((req, res) => {
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+  const uid = req.query.uid;
+  
+  var result = {};
+  
+  var sessions = admin.database().ref('/' + uid + '/sessions');
+  sessions.once('value').then((snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const key = childSnapshot.key;
+      const val = childSnapshot.val();
+      
+      if (startDate <= val.start_date && val.start_date <= endDate) {
+        result[key] = val;
+      }
+    });
+    res.send(result);
+    return true;
+  }).catch((error) => {
+    
+  });
+});
