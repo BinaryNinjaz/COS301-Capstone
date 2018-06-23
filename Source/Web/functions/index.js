ec9103119f09a66cad8d7c4b7e27bf2545b797e6
Letanyan <letanyan.a@gmail.com>
2018-06-24 01:39:20 +0200

    flattend sessions

--- Source/Web/functions/index.js
+++ Source/Web/functions/index.js
@@ -3,9 +3,7 @@ const admin = require('firebase-admin');
 //const cors = require('cors')({origin: true});
 admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
+// ?startDate=[Double]&endDate=[Double]&uid=[String]
 exports.sessionsWithinDates = functions.https.onRequest((req, res) => {
   const startDate = req.query.startDate;
   const endDate = req.query.endDate;
@@ -22,6 +20,39 @@ exports.sessionsWithinDates = functions.https.onRequest((req, res) => {
       if (startDate <= val.start_date && val.start_date <= endDate) {
         result[key] = val;
       }
+    });
+    res.send(result);
+    return true;
+  }).catch((error) => {
+    
+  });
+});
+
+// ?pageNo=[Int]&pageSize=[Int? = 50]&uid=[String]
+exports.flattendSessions = functions.https.onRequest((req, res) => {
+  const pageNo = req.query.pageNo;
+  var pageSize = req.query.pageSize;
+  if (pageSize === undefined) {
+    pageSize = 50;
+  }
+  const uid = req.query.uid;
+  
+  var result = [];
+  var count = 0;
+  var sessions = admin.database().ref('/' + uid + '/sessions').orderByChild('start_date');
+  sessions.once('value').then((snapshot) => {
+    const total = snapshot.numChildren();
+    
+    snapshot.forEach((childSnapshot) => {
+      if (count >= total - (pageNo - 1) * pageSize) {
+        res.send(result);
+        return;
+      } else if (count >= total - pageNo * pageSize) {
+        const key = childSnapshot.key;
+        const val = childSnapshot.val();
+        result.unshift({key: key, startDate: val.start_date});
+      } 
+      count++;
     });
     res.send(result);
     return true;
