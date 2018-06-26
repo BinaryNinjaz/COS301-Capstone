783a3b8689f206063fd4453e2874bfdc21609dc8
Letanyan Arumugam <letanyan.a@gmail.com>
2018-06-26 12:51:58 +0200

    shallow sessions (#159)

--- Source/Web/functions/index.js
+++ Source/Web/functions/index.js
@@ -3,9 +3,7 @@ const admin = require('firebase-admin');
 //const cors = require('cors')({origin: true});
 admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
+// ?startDate=[Double]&endDate=[Double]&uid=[String]
 exports.sessionsWithinDates = functions.https.onRequest((req, res) => {
   const startDate = req.query.startDate;
   const endDate = req.query.endDate;
@@ -28,6 +26,37 @@ exports.sessionsWithinDates = functions.https.onRequest((req, res) => {
   }).catch((error) => {
     
   });
+});
+
+// ?pageNo=[Int]&pageSize=[Int? = 50]&uid=[String]
+exports.flattendSessions = functions.https.onRequest((req, res) => {
+  const pageNo = req.query.pageNo;
+  var pageSize = req.query.pageSize;
+  if (pageSize === undefined) {
+    pageSize = 50;
+  }
+  const uid = req.query.uid;
+  
+  var result = [];
+  var count = 0;
+  var sessions = admin.database().ref('/' + uid + '/sessions').orderByChild('start_date');
+  sessions.once('value').then((snapshot) => {
+    const total = snapshot.numChildren();
+    
+    snapshot.forEach((childSnapshot) => {
+      if (count >= total - (pageNo - 1) * pageSize) {
+        res.send(result);
+        return;
+      } else if (count >= total - pageNo * pageSize) {
+        const key = childSnapshot.key;
+        const val = childSnapshot.val();
+        result.unshift({key: key, start_date: val.start_date, wid: val.wid});
+      } 
+      count++;
+    });
+    res.send(result);
+    return true;
+  }).catch((error) => {});
 });
 
 // Point: [x: Float, y: Float]
