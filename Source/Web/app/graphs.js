bd253cfda968a862adb6104cfa0757e8ce482539
ShaunYates <36733817+ShaunYates@users.noreply.github.com>
2018-07-10 14:23:51 +0200

    fixed a bug (but now has other bug)

--- Source/Web/app/graphs.js
+++ Source/Web/app/graphs.js
@@ -11,16 +11,41 @@ const userID = function() {
   }
 }
 
-function getWorkers(callback) {
-  const ref = firebase.database().ref('/' + userID() + '/workers');
-  ref.once('value').then((snapshot) => {
-    callback(snapshot);
+function yieldsRef() {
+  return database.ref('/' + userID() + '/sessions');
+}
+
+function workersRef() {
+  return database.ref('/' + userID()  + '/workers');
+}
+
+var foremen = [];
+var workers = [];
+function getWorkers() {
+  workersRef().once('value').then(function (snapshot) {
+      snapshot.forEach(function (worker) {
+        console.log("Worker being added.");
+        const w = worker.val();
+        const k = worker.key;
+        if (w.type === "Foreman") {
+          foremen.push({key: k, value: w});
+        } else {
+          workers.push({key: k, value: w});
+        }
+      });
   });
 }
 
-function yieldsRef() {
-  return firebase.database().ref('/' + userID() + '/sessions');
+function getCollectionsRef(sessionKey) {
+  return database.ref('/' + userID() + '/sessions/'+
+                sessionKey+'/collections');
 }
+
+function workerCollections(sessionKey,workerKey) {
+  return database.ref('/' + userID() + '/sessions/'+
+                sessionKey+'/collections/'+workerKey);
+}
+
 
 function foremanForKey(key) {
   for (var k in foremen) {
@@ -40,23 +65,9 @@ function workerForKey(key) {
   return undefined;
 }
 
-var foremen = [];
-var workers = []
-function initPage() {
-  getWorkers((workersSnap) => {
-    foremen = [];
-    workers = [];
-    workersSnap.forEach((worker) => {
-      const w = worker.val();
-      const k = worker.key;
-      if (w.type === "Foreman") {
-        foremen.push({key: k, value: w});
-      } else {
-        workers.push({key: k, value: w});
-      }
-    });
+function initPage(){
+    getWorkers();
     loadSessions();
-  });
 }
 
 /*
@@ -71,15 +82,14 @@ function loadSessions() {
   yieldsRef().once('value').then(function (snapshot) {
       snapshot.forEach(function (child) {
           var childData = child.val();
-          sessionKey = childData.key;
+          sessionKey = child.key;
           var startDate = new Date(childData.start_date * 1000);
           var endDate = new Date(childData.end_date * 1000);
-          sessions.push({start: startDate, end: endDate});
-          const collectionsRef =  firebase.database().ref('/' + userID() + '/sessions/'+
-                sessionKey+'/collections/');  
-          collections.push(populateCollection(collectionsRef));
+          sessions.push({start: startDate, end: endDate});  
+          collections.push(populateCollection(sessionKey));
           ++sessionCount;
       });
+      test();
       calculateBagsPerHour();
   });
 }
@@ -88,29 +98,56 @@ function loadSessions() {
 Object returned from the function below will contain all of the workers' names of
 a specific session and all the dates of the collectons that the workers made 
 */
-function populateCollection(ref) {
+function populateCollection(sessionKey) {
   var obj = [];
-  ref.once('value').then(function (snapshot) {
+  const collectionsRef = getCollectionsRef(sessionKey);
+  collectionsRef.once('value').then(function (snapshot) {
       snapshot.forEach(function (child) {
-        const worker = workerForKey(child.key);
+        var childData = child.val();
+        workerKey = child.key;
+        const worker = workerForKey(workerKey);
         if (worker !== undefined) {
             const name = worker.value.name + " " + worker.value.surname;
+            console.log(name);
             var collectionDates = [];
-            const workerCollRef = ref+'/'+child.key;
+            const workerCollRef = workerCollections(sessionKey,workerKey);
             var count = 0;
             workerCollRef.once('value').then(function (snapshot2) {
                 snapshot2.forEach(function (child2) {
+                    collectionDates[count] = {};
                     collectionDates[count] = new Date(child2.date * 1000);
                     ++count;
                 });
             });
             obj.push({key: name, collections: collectionDates});
+        }else{
+            console.log("Worker is undefined..");
         }
       });
   }); 
   return obj;
 }
 
+var totalBagsPerHour = [];
+var workerBagsPerHour = [];
 function calculateBagsPerHour(){
    //yet to be implemented 
+}
+
+function test(){
+    var count = 0;
+    collections.forEach(function (x){
+        var string = "Collection number: "+count+"\n";
+        console.log(string);
+        x.forEach(function (k){
+            var temp = "Worker: "+k.key+"Dates are below: \\n";
+            console.log(temp);
+            k.collections.forEach(function (d){
+                 temp+=d+" ";   
+            });
+            string+=temp;
+        });
+        window.alert(string);
+        ++count;
+    });
 }
