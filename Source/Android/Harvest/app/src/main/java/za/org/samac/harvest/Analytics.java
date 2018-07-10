c8092a3bf9cde67eaec621dc32deabeca96a4281
John <u15096794@tuks.co.za>
2018-07-10 12:41:24 +0200

    Added radar graph

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/Analytics.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/Analytics.java
@@ -103,7 +103,7 @@ public class Analytics extends AppCompatActivity {
         perSesWorkerComparison.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Analytics.this, BarGraph.class);
+                Intent intent = new Intent(Analytics.this, SpiralGraph.class);
                 startActivity(intent);
             }
         });
