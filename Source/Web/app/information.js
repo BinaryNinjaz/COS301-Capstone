3d3c97e0395824132090223e031ecca1eea4389d
Sizo <u15245579@tuks.co.za>
2018-07-13 21:30:29 +0200

    Added phone number field

--- Source/Web/app/information.js
+++ Source/Web/app/information.js
@@ -613,6 +613,9 @@ function dispWork(id) {
 	"<div class='form-group'><label class='control-label col-sm-2' for='text'>Identity Number:</label>" +
 	"<div class='col-sm-9'><input type='text' class='form-control' id='workID'></div> </div>" +
         "" +
+	"<div class='form-group'><label class='control-label col-sm-2' for='text'>Phone Number:</label>" +
+	"<div class='col-sm-9'><input type='text' class='form-control' id='workContactNo'></div> </div>" +
+        "" +
         "<div class='form-group'><label class='control-label col-sm-2' for='sel1'>Assigned Orchard:</label>" +
         "<div class='col-sm-9'><select class='form-control' id='workOrch'></select></div></div>" +
         "" +
@@ -653,6 +656,9 @@ function dispWork(id) {
 		    "" +
                     "<div class='form-group'><label class='control-label col-sm-2' for='text'>Identity Number:</label>" +
                     "<div class='col-sm-9'><p class='form-control-static'>" + snapshot.val().identityNo + "</p> </div> </div>" +
+		    "" +
+		    "<div class='form-group'><label class='control-label col-sm-2' for='text'>Phone Number:</label>" +
+                    "<div class='col-sm-9'><p class='form-control-static'>" + snapshot.val().phoneNo + "</p> </div> </div>" +
 		    "" +
                     "<div class='form-group'><label class='control-label col-sm-2' for='sel1'>Assigned Orchard:</label>" +
                     "<div class='col-sm-9'><span id='workOrchDisp'></span></div></div>" +
