287d4f232ae57d8cb2e5f8efbb4193bd5c9a09de
ShaunYates <36733817+ShaunYates@users.noreply.github.com>
2018-07-16 14:49:00 +0200

    moved a couple lines around

--- Source/Web/app/graphs.js
+++ Source/Web/app/graphs.js
@@ -71,9 +71,66 @@ function workerForKey(key) {
 }
 
 //calls functions that populate the drop down lists and worker/orchard arrays
+//also draws initial graphs on start up
 function initPage(){
     initOrchards(); //This function initiates the orchards immediately when the analytics page is accessed (for selction)
     initWorkers();	//This function initiates the workers immediately when the analytics page is accessed (for selection)
+
+    //Vincent started working on this function from here
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+            type: 'radar',
+            data: {
+                    //The size of the labels array will depend on the start date and the end date.
+                    //The labels will be the dates, from the specified start till the specified end.
+                    labels: ['Sunday','Monday','Tuesday','Wednesday','Thursady','Friday','Saturday'], //This will contain the dates plotted on each point. (give me the dates)
+                    datasets: [{
+                    label: "Number of Bags p/day", //These are the number of bags per day since the start date and the end date
+
+                    //The following values in the data array are the number of bags collected each day. from start date to end date
+                    data: [4, 0, 5, 0, 0, 0, 4], //The size of this will also depend on the start and the end date
+                    backgroundColor: '#4CAF50' //Color of the area 
+            }]
+            }
+    });
+    
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Period", "Number of Bags", { role: "style" } ], //This line explains the format of the array object
+        ["06:00 - 07:00", 8, "#4CAF50"], //[label: which is the period of 1 hour, Number of bags, color of bar]
+        ["07:00 - 08:00", 6, "#4CAF50"],
+        ["08:00 - 09:00", 3, "#4CAF50"],
+		["09:00 - 10:00", 7, "#4CAF50"],
+        ["10:00 - 11:00", 5, "#4CAF50"],
+        ["11:00 - 12:00", 9, "#4CAF50"],
+		["12:00 - 13:00", 7, "#4CAF50"],
+        ["13:00 - 14:00", 7, "#4CAF50"],
+        ["14:00 - 15:00", 9, "#4CAF50"],
+        ["15:00 - 16:00", 8, "#4CAF50"],
+		["16:00 - 17:00", 11, "#4CAF50"],
+        ["17:00 - 18:00", 15, "#4CAF50"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+      var nameOfWorker; //This has to be assigned the name of the worker/the id ow the worker
+      var options = {
+        title: "Number of bags collected per hour by Teboho Mokoena", //Teboho Mokoena will be replaced with 'nameOfWorker' variable
+        width: 1200, //Setting the width 
+        height: 500, //Setting the height
+        bar: {groupWidth: "95%"}, //This is the grouping width of the bar graph
+        legend: { position: "none" }, //This will be determined by the UX designer
+      };
+      var chart = new google.visualization.BarChart(document.getElementById("curve_chart"));
+      chart.draw(view, options);
+  }
 }
 
 /* This function loads all available orchards in the database, for graph filtering */
@@ -187,28 +244,6 @@ function orchardPerformance(start, end, id){
    var params = constructParams(groupBy,period,startDate,endDate,uid);
    var response = sendPostRequest(params);
    //implementation will go here
-   
-   
-   
-   
-   
-   //Vincent started working on this function from here
-   var ctx = document.getElementById("myChart").getContext('2d');
-	var myChart = new Chart(ctx, {
-		type: 'radar',
-		data: {
-			//The size of the labels array will depend on the start date and the end date.
-			//The labels will be the dates, from the specified start till the specified end.
-			labels: ['Sunday','Monday','Tuesday','Wednesday','Thursady','Friday','Saturday'], //This will contain the dates plotted on each point. (give me the dates)
-			datasets: [{
-			label: "Number of Bags p/day", //These are the number of bags per day since the start date and the end date
-			
-			//The following values in the data array are the number of bags collected each day. from start date to end date
-			data: [4, 0, 5, 0, 0, 0, 4], //The size of this will also depend on the start and the end date
-			backgroundColor: '#4CAF50' //Color of the area 
-		}]
-		}
-	});
 }
 
 //updates worker graph based on user input
@@ -221,50 +256,7 @@ function workerPerformance(start, end, id){
    uid = id;
    var params = constructParams(groupBy,period,startDate,endDate,uid);
    var response = sendPostRequest(params);
-   //implementation will go here
-   
-   
-   
-   
-   
-   //Vincent started working on this function from here
-       google.charts.load("current", {packages:["corechart"]});
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ["Period", "Number of Bags", { role: "style" } ], //This line explains the format of the array object
-        ["06:00 - 07:00", 8, "#4CAF50"], //[label: which is the period of 1 hour, Number of bags, color of bar]
-        ["07:00 - 08:00", 6, "#4CAF50"],
-        ["08:00 - 09:00", 3, "#4CAF50"],
-		["09:00 - 10:00", 7, "#4CAF50"],
-        ["10:00 - 11:00", 5, "#4CAF50"],
-        ["11:00 - 12:00", 9, "#4CAF50"],
-		["12:00 - 13:00", 7, "#4CAF50"],
-        ["13:00 - 14:00", 7, "#4CAF50"],
-        ["14:00 - 15:00", 9, "#4CAF50"],
-        ["15:00 - 16:00", 8, "#4CAF50"],
-		["16:00 - 17:00", 11, "#4CAF50"],
-        ["17:00 - 18:00", 15, "#4CAF50"]
-      ]);
-
-      var view = new google.visualization.DataView(data);
-      view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
-      var nameOfWorker; //This has to be assigned the name of the worker/the id ow the worker
-      var options = {
-        title: "Number of bags collected per hour by Teboho Mokoena", //Teboho Mokoena will be replaced with 'nameOfWorker' variable
-        width: 1200, //Setting the width 
-        height: 500, //Setting the height
-        bar: {groupWidth: "95%"}, //This is the grouping width of the bar graph
-        legend: { position: "none" }, //This will be determined by the UX designer
-      };
-      var chart = new google.visualization.BarChart(document.getElementById("curve_chart"));
-      chart.draw(view, options);
-  }
+   //implementation will go here  
 }
 
 //creates the parameter string for the http post request
