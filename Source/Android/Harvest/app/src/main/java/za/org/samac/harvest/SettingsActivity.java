f99b242314c5575e4b6859b4bfd5dce7a6c9aaf7
u15008739 <u15008739@tuks.co.za>
2018-07-19 10:29:45 +0200

    Reauthentication now works.

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SettingsActivity.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SettingsActivity.java
@@ -19,7 +19,9 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -28,6 +30,7 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.auth.UserInfo;
@@ -37,6 +40,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import static android.content.ContentValues.TAG;
@@ -196,6 +200,13 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
         private boolean isFarmer;
 
+        enum AccountAction{
+            EMAIL,
+            PASSWORD,
+            DELETE
+        }
+        AccountAction accountAction;
+
         private PreferenceScreen preferenceScreen;
 
         @Override
@@ -253,22 +264,17 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                     }
                 });
 
+                /*
+                If user need to reauth, then if(reAuth()){repeat action}
+                TODO: Perhaps give user 3? chances, and if they are used up, then sign the user out.
+                 */
+
                 //Update Email
                 //TODO: Verification email
                 findPreference(getString(R.string.pref_account_email_key)).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                     @Override
                     public boolean onPreferenceChange(Preference preference, final Object newValue) {
-                        user.updateEmail(newValue.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()){
-                                    getActivity().setTitle(newValue.toString());
-                                    userAdmin.child("email").setValue(newValue.toString());
-                                    Log.d(TAG, "User email updated.");
-                                }
-                            }
-                        });
-
+                        Email(newValue.toString());
                         return false;
                     }
                 });
@@ -277,13 +283,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 findPreference(getString(R.string.pref_account_password_key)).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                     @Override
                     public boolean onPreferenceChange(Preference preference, Object newValue) {
-                        user.updatePassword(newValue.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Log.d(TAG, "User password updated.");
-                            }
-                        });
-
+                        Password(newValue.toString());
                         return false;
                     }
                 });
@@ -325,55 +325,43 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 findPreference(getString(R.string.pref_account_delete_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                     @Override
                     public boolean onPreferenceClick(Preference preference) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-                        builder.setMessage(R.string.pref_account_delete_alert_message)
-                                .setTitle(R.string.pref_account_delete_alert_title)
-                                .setPositiveButton(R.string.pref_account_delete_alert_yes, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        //Another confirmation dialog
-                                        AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
+                            builder.setMessage(R.string.pref_account_delete_alert_message)
+                                    .setTitle(R.string.pref_account_delete_alert_title)
+                                    .setPositiveButton(R.string.pref_account_delete_alert_yes, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            //Another confirmation dialog
+                                            AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
 
-                                        builder1.setTitle(R.string.pref_account_delete_alert_sure_title)
-                                                .setMessage(R.string.pref_account_delete_alert_sure_message)
-                                                .setPositiveButton(R.string.pref_account_delete_alert_sure_yes, new DialogInterface.OnClickListener() {
-                                                    @Override
-                                                    public void onClick(DialogInterface dialog, int which) {
-                                                        FirebaseAuth.getInstance().getCurrentUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                            @Override
-                                                            public void onComplete(@NonNull Task<Void> task) {
-                                                                if (task.isSuccessful()){
-                                                                    database.getReference("/" + user.getUid() + "/").setValue(null);
-                                                                    Log.d(TAG, "User account deleted");
-                                                                    startActivity(new Intent(getActivity(), SignIn_Choose.class));
-                                                                    getActivity().finish();
-                                                                }
-                                                                else {
-                                                                    Log.e(TAG, "User account deletion failed");
-                                                                }
-                                                            }
-                                                        });
-                                                    }
-                                                })
-                                                .setNegativeButton(R.string.pref_account_delete_alert_sure_no, new DialogInterface.OnClickListener() {
-                                                    @Override
-                                                    public void onClick(DialogInterface dialog, int which) {
-                                                        dialog.cancel();
-                                                    }
-                                                });
+                                            builder1.setTitle(R.string.pref_account_delete_alert_sure_title)
+                                                    .setMessage(R.string.pref_account_delete_alert_sure_message)
+                                                    .setPositiveButton(R.string.pref_account_delete_alert_sure_yes, new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which) {
+                                                            Delete();
+                                                        }
+                                                    })
+                                                    .setNegativeButton(R.string.pref_account_delete_alert_sure_no, new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which) {
+                                                            dialog.cancel();
+                                                        }
+                                                    })
+                                            ;
 
-                                        builder1.show();
-                                    }
-                                })
-                                .setNegativeButton(R.string.pref_account_delete_alert_no, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
+                                            builder1.show();
+                                        }
+                                    })
+                                    .setNegativeButton(R.string.pref_account_delete_alert_no, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                        }
+                                    });
 
-                        builder.show();
+                            builder.show();
 
                         return true;
                     }
@@ -388,21 +376,133 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 //            bindPreferenceSummaryToValue(findPreference("example_list"));
         }
 
-        /**
-         * Reauthenticate the user.
-         * @return true if the authentication was successful, false if the user canceled.
-         */
-        private boolean reAuth(){
-
+        private void Delete(){
+            FirebaseAuth.getInstance().getCurrentUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        database.getReference("/" + user.getUid()).setValue(null);
+                        Log.d(TAG, "User account deleted");
+                        startActivity(new Intent(getActivity(), SignIn_Choose.class));
+                        getActivity().finish();
+                    } else {
+                        Log.e(TAG, "User account deletion failed " + task.getException().toString());
+                        try {
+                            throw task.getException();
+                        } catch (FirebaseAuthRecentLoginRequiredException e) {
+                            accountAction = AccountAction.DELETE;
+                            reAuth(false, null);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
         }
 
-        /**
-         * Show a dialog to get account information from the user.
-         * @param again true if the user is trying again.
-         * @return what the user entered. [email, password]
-         */
-        private String[] askReAuth(boolean again){
+        public void Email(final String email){
+            user.updateEmail(email.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        getActivity().setTitle(email.toString());
+                        userAdmin.child("email").setValue(email.toString());
+                        Log.d(TAG, "User email updated.");
+                    }
+                    else {
+                        try {
+                            throw task.getException();
+                        }
+                        catch (FirebaseAuthRecentLoginRequiredException e){
+                            accountAction = AccountAction.EMAIL;
+                            reAuth(false, email);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
 
+        public void Password(final String password){
+            user.updatePassword(password.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "User password updated.");
+                    }
+                    else {
+                        try{
+                            throw task.getException();
+                        }
+                        catch (FirebaseAuthRecentLoginRequiredException e){
+                            accountAction = AccountAction.PASSWORD;
+                            reAuth(false, password);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+
+
+        /**
+         * Reauthenticate the user.
+         * @param again true if the user is trying again.
+         */
+        private void reAuth(boolean again, final String passAlong){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            builder.setView(inflater.inflate(R.layout.dialog_reauth, null))
+                    .setPositiveButton(R.string.pref_account_reAuthConfirm, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+//                            EditText email = dialog.findViewById(R.id.pref_account_reAuth_email);
+//                            EditText pass  = getActivity().findViewById(R.id.pref_account_reAuth_password);
+                            EditText email = ((AlertDialog) dialog).findViewById(R.id.pref_account_reAuth_email);
+                            EditText pass = ((AlertDialog) dialog).findViewById(R.id.pref_account_reAuth_password);
+//                            result = new String[]{email.getText().toString(), pass.getText().toString()};
+                            AuthCredential credential = EmailAuthProvider.getCredential(email.getText().toString(), pass.getText().toString());
+                            user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Log.d(TAG, "User re-authenticated.");
+                                        switch (accountAction){
+                                            case DELETE:
+                                                Delete();
+                                                break;
+                                            case EMAIL:
+                                                Email(passAlong);
+                                                break;
+                                            case PASSWORD:
+                                                Password(passAlong);
+                                                break;
+                                        }
+                                    }
+                                    else {
+                                        reAuth(true, null);
+                                    }
+                                }
+                            });
+                        }
+                    })
+                    .setNegativeButton(R.string.pref_account_reAuthCancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    })
+            ;
+            if (again){
+                builder.setTitle(R.string.pref_account_reAuthAgain);
+            }
+            else {
+                builder.setTitle(R.string.pref_account_reAuth);
+            }
+            builder.show();
         }
 
         @Override
