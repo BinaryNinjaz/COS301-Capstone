b64d9d47702817fa6d7ee46b37867da69fe66a81
Letanyan <letanyan.a@gmail.com>
2018-07-21 22:18:14 +0200

    native session paging

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SessionItem.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SessionItem.java
@@ -4,12 +4,6 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class SessionItem {
-    public static class Selection {
-        public String key;
-        public Date startDate;
-        public String foreman;
-    }
-
     public String key;
     public Date startDate;
     public Date endDate;
