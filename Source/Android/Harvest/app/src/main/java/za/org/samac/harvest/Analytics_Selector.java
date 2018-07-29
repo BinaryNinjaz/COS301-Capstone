1baa6a1ac3cf2b67537a1be7379d73687b2342f7
u15008739 <u15008739@tuks.co.za>
2018-07-29 08:56:12 +0200

    Add dividers to selector

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/Analytics_Selector.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/Analytics_Selector.java
@@ -1,8 +1,12 @@
 package za.org.samac.harvest;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -57,6 +61,7 @@ public class Analytics_Selector extends Fragment{
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new Analytics_Selector_ItemDivider(getContext()));
 
         if (Data.isPulling()){
             swipeRefreshLayout.setRefreshing(true);
@@ -83,19 +88,45 @@ public class Analytics_Selector extends Fragment{
     }
 }
 
+class Analytics_Selector_ItemDivider extends RecyclerView.ItemDecoration{
+    private Drawable divider;
+
+    public Analytics_Selector_ItemDivider(Context context){
+//        divider = context.getResources().getDrawable(R.drawable.line_divider);
+        divider = ContextCompat.getDrawable(context, R.drawable.line_divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i ++){
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + divider.getIntrinsicHeight();
+
+            divider.setBounds(left, top, right, bottom);
+            divider.draw(c);
+        }
+    }
+}
+
 class Analytics_Selector_Adapter extends RecyclerView.Adapter<Analytics_Selector_Adapter.ViewHolder>{
 
     private List<DBInfoObject> items;
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         public CheckBox checkBox;
-        public TextView textView;
 
         public ViewHolder(View view){
             super(view);
 
             checkBox = view.findViewById(R.id.anal_itemCheckBox);
-            textView = view.findViewById(R.id.anal_itemText);
         }
     }
 
@@ -126,8 +157,8 @@ class Analytics_Selector_Adapter extends RecyclerView.Adapter<Analytics_Selector
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.textView.setText(items.get(position).toString());
         holder.checkBox.setTag(items.get(position).getId());
+        holder.checkBox.setText(items.get(position).toString());
     }
 
     @Override
