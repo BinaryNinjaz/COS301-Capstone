5b33d59089eee18be144a8c3a6b5a60e8fbaaf6e
ShaunYates <36733817+ShaunYates@users.noreply.github.com>
2018-08-03 21:42:38 +0200

    adding temporary file

---
+++ Source/Web/app/Settings.js
@@ -0,0 +1,25 @@
+const database = firebase.database();	/* Pointing to database on firebase cloud */
+const user = function() { return firebase.auth().currentUser }; /* Function which authenticates user */
+
+/* Function returns the user ID of the selected user */
+const userID = function() {
+  if (user() !== null) {
+    return user().uid ;
+  } else {
+    return "";
+  }
+}
+
+$(window).bind("load", () => {
+  let succ = () => {
+    initPage();
+  };
+  let fail = () => {
+    
+  };
+  retryUntilTimeout(succ, fail, 1000);
+});
+
+function initPage(){
+    
+}
