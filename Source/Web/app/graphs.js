9fae5b82a1e501b886e0bed07d3998d22c4f73b9
Tebza17 <tebza17.tm@gmail.com>
2018-08-04 19:27:03 +0200

    Adding a spinner for workers potential

--- Source/Web/app/graphs.js
+++ Source/Web/app/graphs.js
@@ -389,7 +389,7 @@ function formatDate(date) {
     return [year, month, day].join('-');
 }
 /* This function shows the spinner while still waiting for resources*/
-var spinner;
+var spinnerOrchard;
 function updateSpinerOrchard(shouldSpin) {
   var opts = {
 	lines: 8, // The number of lines to draw
@@ -415,11 +415,48 @@ function updateSpinerOrchard(shouldSpin) {
   var target = document.getElementById('myChart');
   var button = document.getElementById('updateButton');
   if (shouldSpin) {
-	spinner = new Spinner(opts).spin(target);
+	spinnerOrchard = new Spinner(opts).spin(target);
 	button.style.visibility = "hidden";
   } else {
 	button.style.visibility = "visible";
-	spinner.stop();
-	spinner = null;
+	spinnerOrchard.stop();
+	spinnerOrchard = null;
+  }
+}
+
+
+/* This function shows the spinner while still waiting for resources*/
+var spinnerOrchard;
+function updateSpinerOrchard(shouldSpin) {
+  var opts = {
+	lines: 8, // The number of lines to draw
+	length: 37, // The length of each line
+	width: 10, // The line thickness
+	radius: 45, // The radius of the inner circle
+	scale: 1, // Scales overall size of the spinner
+	corners: 1, // Corner roundness (0..1)
+	color: '#4CAF50', // CSS color or array of colors
+	fadeColor: 'transparent', // CSS color or array of colors
+	speed: 1, // Rounds per second
+	rotate: 0, // The rotation offset
+	animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+	direction: 1, // 1: clockwise, -1: counterclockwise
+	zIndex: 2e9, // The z-index (defaults to 2000000000)
+	className: 'spinner', // The CSS class to assign to the spinner
+	top: '50%', // Top position relative to parent
+	left: '50%', // Left position relative to parent
+	shadow: '0 0 1px transparent', // Box-shadow for the lines
+	position: 'absolute' // Element positioning
+  };
+
+  var target = document.getElementById('myChart');
+  var button = document.getElementById('updateButton');
+  if (shouldSpin) {
+	spinnerOrchard = new Spinner(opts).spin(target);
+	button.style.visibility = "hidden";
+  } else {
+	button.style.visibility = "visible";
+	spinnerOrchard.stop();
+	spinnerOrchard = null;
   }
 }
