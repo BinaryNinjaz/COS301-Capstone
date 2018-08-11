fdc895c38bbf34ef647d4c9fc0f8494f9d85395c
John <u15096794@tuks.co.za>
2018-08-11 18:10:56 +0200

    Added tutorial

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SignIn_SignUp.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SignIn_SignUp.java
@@ -222,7 +222,7 @@ public class SignIn_SignUp extends AppCompatActivity implements LoaderCallbacks<
                             new Handler().postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Intent intent = new Intent(SignIn_SignUp.this, InformationActivity.class);
+                                    Intent intent = new Intent(SignIn_SignUp.this, ViewFlipperActivity.class);
                                     startActivity(intent);
                                     finish();//kill current Activity
                                 }
