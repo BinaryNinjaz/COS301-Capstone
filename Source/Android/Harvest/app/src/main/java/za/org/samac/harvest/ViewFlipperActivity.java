14680beaf9439bac7eb44fe221e82d1c3288b5f3
John <u15096794@tuks.co.za>
2018-08-16 11:12:23 +0200

    Made flipper more user friendly

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/ViewFlipperActivity.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/ViewFlipperActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -24,6 +25,7 @@ public class ViewFlipperActivity extends Activity {
     private TextView skipThree;
     private TextView skipFour;
     private TextView done;
+    private Integer slideCount = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -92,6 +94,15 @@ public class ViewFlipperActivity extends Activity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        Log.d("CDA", "onBackPressed Called");
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+    }
+
     private void endThis(boolean goToStart){
         if (goToStart) {
             Intent intent = new Intent(ViewFlipperActivity.this, InformationActivity.class);
@@ -106,14 +117,29 @@ public class ViewFlipperActivity extends Activity {
             try {
                 // right to left swipe
                 if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                    mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_in));
-                    mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_out));
-                    mViewFlipper.showNext();
+                    if (slideCount == 4) {
+                        slideCount = 0;
+                        Intent intent = new Intent(ViewFlipperActivity.this, InformationActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_in));
+                        mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_out));
+                        mViewFlipper.showNext();
+                        slideCount++;
+                    }
+
                     return true;
                 } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-                    mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.right_in));
-                    mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext,R.anim.right_out));
-                    mViewFlipper.showPrevious();
+                    if (slideCount == 0) {
+
+                    } else {
+                        mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.right_in));
+                        mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext,R.anim.right_out));
+                        mViewFlipper.showPrevious();
+                        slideCount--;
+                    }
+
                     return true;
                 }
 
