413870b5ad01f6592ae7e39dd4d1e5a593063c8f
John <u15096794@tuks.co.za>
2018-08-27 20:56:43 +0200

    Fixed Stats log out

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SessionsMap.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/SessionsMap.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -196,6 +198,13 @@ public class SessionsMap extends AppCompatActivity implements OnMapReadyCallback
             }
             return false;
         }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
 
