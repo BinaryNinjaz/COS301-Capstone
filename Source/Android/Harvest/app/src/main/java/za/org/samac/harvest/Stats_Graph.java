20938f1a48a364adba3e6c207ab0fa1b71800c3f
u15008739 <u15008739@tuks.co.za>
2018-08-28 09:53:20 +0200

    Better graph screen rotation

--- Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/Stats_Graph.java
+++ Source/Android/Harvest/app/src/main/java/za/org/samac/harvest/Stats_Graph.java
@@ -96,9 +96,6 @@ public class Stats_Graph extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //Remove notification bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats_graph);
 
